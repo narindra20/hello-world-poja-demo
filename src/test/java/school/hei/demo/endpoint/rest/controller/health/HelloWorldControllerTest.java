ea69f525ebeee0641f108a7432aa01dd482f0d5b
narindra20 <hei.narindra@gmail.com>
2026-05-25 19:22:42 +0300

    feat: add hello world endpoint

---
+++ src/test/java/school/hei/demo/endpoint/rest/controller/health/HelloWorldControllerTest.java
@@ -0,0 +1,13 @@
+package school.hei.demo.endpoint.rest.controller.health;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloWorldControllerTest {
+
+    @Test
+    void hello() {
+        assertEquals("Hello World!", new HelloWorldController().hello());
+    }
+}
